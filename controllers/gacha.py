094853c9ee473a603c5573b5e59ae9c8f5861a19
Thejairex <yairjesus777@gmail.com>
2024-11-23 14:50:41 -0300

    Re ajuts the probs of stars

--- controllers/gacha.py
+++ controllers/gacha.py
@@ -22,26 +22,26 @@ class Gacha:
     def roll(self):
         self.count_rolls += 1
 
-        if self.count_rolls % 10 == 0:
-            rarity = 3
+        if self.count_rolls % 90 == 0:
+            rarity = 5
         elif self.count_rolls % 25 == 0:
             rarity = 4
-        elif self.count_rolls % 50 == 0:
-            rarity = 5
+        elif self.count_rolls % 10 == 0:
+            rarity = 3
         else:
             rarity = self._random_roll()
         
-        print(rarity)
         self._ajust_prob()
+        if rarity in [3,4,5]:
+            self._reset_rarity(rarity)
         
         character = self._get_by_star(rarity)
+        print(character.name)
         return character
     
     def _random_roll(self):
         rarity = list(self.probs.keys())
         probs = list(self.probs.values())
-        print(rarity)
-        print(probs)
         return random.choices(rarity, weights=probs, k=1)[0]
     
     def _get_by_star(self, star):
@@ -55,15 +55,24 @@ class Gacha:
         """
         Ajust the weight of 5 stars characters based on the number of rolls
         """
-        rest_roll = 50 - self.count_rolls
+        rest_roll = 90 - self.count_rolls
         if rest_roll > 0:
-            increment = (rest_roll / 50) * 1
-            self.probs[5] = min(self.probs[5] + increment, self.max_probability)  
-            rest_prob = 100 - self.probs[5]
-            self.probs[1] = rest_prob * 0.5
-            self.probs[2] = rest_prob * 0.3
-            self.probs[3] = rest_prob * 0.15
-            self.probs[4] = rest_prob * 0.05
+            increment_5 = (rest_roll / 90) / 90 * 10
+            self.probs[5] = self.probs[5] + increment_5
+            
+            increment_4 = (rest_roll / 90) / 90 * 5
+            self.probs[4] = self.probs[4] + increment_4
+            
+            increment_3 = (rest_roll / 90) / 90 * 3
+            self.probs[3] = self.probs[3] + increment_3
+            
+            probs_rest = 100 - (
+                self.probs[5] +
+                self.probs[4] +
+                self.probs[3] 
+            )
+            self.probs[1] = probs_rest * 0.625 
+            self.probs[2] = probs_rest * 0.375 
 
         # check if the number of sum of probabilities is 100
         print(sum(self.probs.values()))
